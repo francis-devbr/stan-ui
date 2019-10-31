fa20f6429ebf47f1a195fc3c11129b9ddcec1987
Francis Oliveira <francis_devbr@outlook.com>
2019-10-31 18:43:11 -0300

    heroku

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.use(function (req, res, next) {
   
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static('./dist/stan-ui'));
+app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8000)
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname,'/dist/stan-ui/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
