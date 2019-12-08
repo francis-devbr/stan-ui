596658139b6f99e4c3368ba9d44bc127913f3adf
ccarol-freitas <carolfreitas101996@gmail.com>
2019-12-08 07:17:13 -0300

    Atualização de perfil

--- src/app/fake-db/profile.ts
+++ src/app/fake-db/profile.ts
@@ -2,88 +2,39 @@ export class ProfileFakeDb
 {
     public static about = {
         'general': {
-            'gender'   : 'Male',
-            'birthday' : 'February 30th, 1974',
+            'gender'   : 'Masculino',
+            'birthday' : '30 de Março, 1984',
             'locations': [
-                'London, UK',
-                'New York, USA'
+                'São Paulo, SP'
             ],
-            'about'    : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget pharetra felis, sed ullamcorper dui. Sed et elementum neque. Vestibulum pellente viverra ultrices. Etiam justo augue, vehicula ac gravida a, interdum sit amet nisl. Integer vitae nisi id nibh dictum mollis in vitae tortor.'
+            'about'    : 'Sou programador de linguagens para internet há uns 19 anos, atualmente trabalho como desenvolvedor JAVA na Pim Dev e faço uns freelancers por ai. :)'
         },
         'work'   : {
-            'occupation': 'Developer',
+            'occupation': 'Desenvolvedor',
             'skills'    : 'C#, PHP, Javascript, Angular, JS, HTML, CSS',
             'jobs'      : [
                 {
-                    'company': 'Self-Employed',
-                    'date'   : '2010 - Now'
+                    'company': 'Pim Dev',
+                    'date'   : ' - Novembro - 2019'
                 },
                 {
                     'company': 'Google',
-                    'date'   : '2008 - 2010'
+                    'date'   : '2005 - 2018'
                 }
             ]
         },
         'contact': {
-            'address' : 'Ut pharetra luctus est quis sodales. Duis nisi tortor, bibendum eget tincidunt, aliquam ac elit. Mauris nec euismod odio.',
             'tel'     : [
-                '+6 555 6600',
-                '+9 555 5255'
+                '+55 11 99999 9999',
+                '+55 11 2222 2222'
             ],
             'websites': [
-                'withinpixels.com'
+                'stan.com'
             ],
             'emails'  : [
-                'mail@withinpixels.com',
-                'mail@creapond.com'
+                'jose.alfredo@gmail.com',
+                'jose.alfredo@outlook.com'
             ]
-        },
-        'groups' : [
-            {
-                'name'    : 'Android',
-                'category': 'Technology',
-                'members' : '1.856.546'
-            },
-            {
-                'name'    : 'Google',
-                'category': 'Web',
-                'members' : '1.226.121'
-            },
-            {
-                'name'    : 'Fallout',
-                'category': 'Games',
-                'members' : '526.142'
-            }
-        ],
-        'friends': [
-            {
-                'name'  : 'Garry Newman',
-                'avatar': 'assets/images/avatars/garry.jpg'
-            },
-            {
-                'name'  : 'Carl Henderson',
-                'avatar': 'assets/images/avatars/carl.jpg'
-            },
-            {
-                'name'  : 'Jane Dean',
-                'avatar': 'assets/images/avatars/jane.jpg'
-            },
-            {
-                'name'  : 'Garry Arnold',
-                'avatar': 'assets/images/avatars/garry.jpg'
-            },
-            {
-                'name'  : 'Vincent Munoz',
-                'avatar': 'assets/images/avatars/vincent.jpg'
-            },
-            {
-                'name'  : 'Alice Freeman',
-                'avatar': 'assets/images/avatars/alice.jpg'
-            },
-            {
-                'name'  : 'Andrew Green',
-                'avatar': 'assets/images/avatars/andrew.jpg'
-            }
-        ]
+        }
     };
 }
