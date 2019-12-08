5935fb589df8b51159f9c61aecc3c6642a62cb46
ccarol-freitas <carolfreitas101996@gmail.com>
2019-12-08 15:55:52 -0300

    atualização

--- src/app/fake-db/frota.ts
+++ src/app/fake-db/frota.ts
@@ -351,4 +351,134 @@ export class FrotaFakeDb {
         }
     ];
 
+    public static viagens = [
+        {
+            'id': '1',
+            'name': 'A Walk Amongst Friends - Canvas Print',
+            'handle': 'a-walk-amongst-friends-canvas-print',
+            'description': 'Officia amet eiusmod eu sunt tempor voluptate laboris velit nisi amet enim proident et. Consequat laborum non eiusmod cillum eu exercitation. Qui adipisicing est fugiat eiusmod esse. Sint aliqua cupidatat pariatur mollit ad est proident reprehenderit. Eiusmod adipisicing laborum incididunt sit aliqua ullamco.',
+            'categories': [
+                'Canvas Print',
+                'Nature'
+            ],
+            'tags': [
+                'canvas-print',
+                'nature'
+            ],
+            'featuredImageId': 1,
+            'images': [
+                {
+                    'id': 0,
+                    'url': 'assets/images/frota/manutencao-image-placeholder',
+                    'type': 'image'
+                },
+                {
+                    'id': 1,
+                    'url': 'assets/images/frota/manutencao-image-placeholder',
+                    'type': 'image'
+                },
+                {
+                    'id': 2,
+                    'url': 'assets/images/frota/manutencao-image-placeholder',
+                    'type': 'image'
+                }
+            ],
+            'priceTaxExcl': 9.309,
+            'priceTaxIncl': 10.24,
+            'taxRate': 10,
+            'comparedPrice': 19.90,
+            'quantity': 3,
+            'sku': 'A445BV',
+            'width': '22',
+            'height': '24',
+            'depth': '15',
+            'weight': '3',
+            'extraShippingFee': 3.00,
+            'active': true
+        },
+        {
+            'id': '2',
+            'name': 'Braies Lake - Canvas Print',
+            'handle': 'braies-lake-canvas-print',
+            'description': 'Duis anim est non exercitation consequat. Ullamco ut ipsum dolore est elit est ea elit ad fugiat exercitation. Adipisicing eu ad sit culpa sint. Minim irure Lorem eiusmod minim nisi sit est consectetur.',
+            'categories': [
+                'Canvas Print',
+                'Nature'
+            ],
+            'tags': [
+                'canvas-print',
+                'nature'
+            ],
+            'featuredImageId': 2,
+            'images': [
+                {
+                    'id': 0,
+                    'url': 'assets/images/frota/manutencao-image-placeholder',
+                    'type': 'image'
+                },
+                {
+                    'id': 1,
+                    'url': 'assets/images/frota/manutencao-image-placeholder',
+                    'type': 'image'
+                }
+            ],
+            'priceTaxExcl': 22.381,
+            'priceTaxIncl': 24.62,
+            'taxRate': 10,
+            'comparedPrice': 29.90,
+            'quantity': 92,
+            'sku': 'A445BV',
+            'width': '22',
+            'height': '24',
+            'depth': '15',
+            'weight': '3',
+            'extraShippingFee': 3.00,
+            'active': true
+        },
+        {
+            'id': '3',
+            'name': 'Fall Glow - Canvas Print',
+            'handle': 'fall-glow-canvas-print',
+            'description': 'Sit ipsum esse eu consequat veniam sit consectetur consectetur anim. Ut Lorem dolor ullamco do. Laboris excepteur consectetur tempor nisi commodo amet deserunt duis.',
+            'categories': [
+                'Canvas Print',
+                'Nature'
+            ],
+            'tags': [
+                'canvas-print',
+                'nature'
+            ],
+            'featuredImageId': 3,
+            'images': [
+                {
+                    'id': 0,
+                    'url': 'assets/images/frota/manutencao-image-placeholder',
+                    'type': 'image'
+                },
+                {
+                    'id': 1,
+                    'url': 'assets/images/frota/manutencao-image-placeholder',
+                    'type': 'image'
+                },
+                {
+                    'id': 2,
+                    'url': 'assets/images/frota/manutencao-image-placeholder',
+                    'type': 'image'
+                }
+            ],
+            'priceTaxExcl': 44.809,
+            'priceTaxIncl': 49.29,
+            'taxRate': 10,
+            'comparedPrice': 59.90,
+            'quantity': 60,
+            'sku': 'A445BV',
+            'width': '22',
+            'height': '24',
+            'depth': '15',
+            'weight': '3',
+            'extraShippingFee': 3.00,
+            'active': true
+        }
+    ];
+
 }
